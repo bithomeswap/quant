1b0e1755c56433090dd0806aaf518b5eecd0b1cb
bithomeswap <74951426+bithomeswap@users.noreply.github.com>
2023-04-03 17:18:01 +0800

    1

--- Z本地计算/区间划分.py
+++ Z本地计算/区间划分.py
@@ -1,19 +1,25 @@
-# 使用等距离的方式进行数据划分。可以使用NumPy中的linspace函数，将数据集的最大值和最小值之间的区间划分为n个部分。
-# 具体实现代码如下：
 import numpy as np
-# 假设数据存储在ndarray类型的变量data中
-data = np.array([1, 3, 5, 6, 8, 9, 10, 12, 15, 18])
-n = 5  # 划分的区间数
-# 对数据进行排序
-sorted_data = np.sort(data)
+import pandas as pd
+
+# 读取'STOCK指标.csv'文件
+df = pd.read_csv('STOCK指标.csv')
+
+# 对'MACDsignal'列进行排序
+sorted_data = np.sort(df['MACDsignal'])
+
+# 划分的区间数为6
+n = 40
+
 # 按照等距离的方式将数据划分成n个部分
-indices = np.linspace(0, len(data), num=n+1, endpoint=True, dtype=int)
+indices = np.linspace(0, len(df['MACDsignal']), num=n+1, endpoint=True, dtype=int)
+
 # 得到每一个区间的上界，并作为该部分对应的区间范围
 ranges = []
 for i in range(len(indices) - 1):
     start_idx = indices[i]
-    end_idx = indices[i+1] if i != len(indices) - 2 else len(data)  # 最后一段需要特殊处理
+    end_idx = indices[i+1] if i != len(indices) - 2 else len(df['MACDsignal'])  # 最后一段需要特殊处理
     upper_bound = sorted_data[end_idx-1]  # 注意索引从0开始，因此要减1
     ranges.append((sorted_data[start_idx], upper_bound))
+
 # 输出结果
 print(ranges)
--- Z本地计算/概率统计.py
+++ Z本地计算/概率统计.py
@@ -8,10 +8,26 @@ df = pd.read_csv(f'{name}指标.csv')
 mubiao = 'MACDsignal'
 print('已经获取数据')
 
+# 对'MACDsignal'列进行排序
+sorted_data = np.sort(df['MACDsignal'])
+
+n = 40  # 划分的区间数为40
+# 按照等距离的方式将数据划分成n个部分
+indices = np.linspace(0, len(df['MACDsignal']),
+                      num=n+1, endpoint=True, dtype=int)
+# 得到每一个区间的上界，并作为该部分对应的区间范围
+ranges = []
+for i in range(len(indices) - 1):
+    start_idx = indices[i]
+    end_idx = indices[i+1] if i != len(indices) - \
+        2 else len(df['MACDsignal'])  # 最后一段需要特殊处理
+    upper_bound = sorted_data[end_idx-1]  # 注意索引从0开始，因此要减1
+    ranges.append((sorted_data[start_idx], upper_bound))
 
 # 计算指标
 for n in range(1, 8):
-    rank_ranges = [(i-50.6, i-49.6) for i in range(100)]
+    # 将划分的区间赋值到待计算的列表当中
+    rank_ranges = ranges
     result_dicts = []
     for rank_range in rank_ranges:
         sub_df = df[df[f'{mubiao}'].between(
