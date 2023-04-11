cfaeea27c03941599cf94fbbaaa524fb6b155eea
wth000 <you@example.com>
2023-04-11 03:14:45 +0100

    1

--- 数据获取/getcoin指标.py
+++ 数据获取/getcoin指标.py
@@ -44,25 +44,25 @@ def get_technical_indicators(df):  # 定义计算技术指标的函数
 
         macd, macdsignal, macdhist = talib.MACD(
             df['收盘'].values, fastperiod=2, slowperiod=n, signalperiod=10)
-        df[f'收盘MACD_1_{n}_10比值'] = macd/df['收盘']
-        df[f'收盘MACDsignal_1_{n}_10比值'] = macdsignal/df['收盘']
-        df[f'收盘MACDhist_1_{n}_10比值'] = macdhist/df['收盘']
+        df[f'收盘MACD_2_{n}_10比值'] = macd/df['收盘']
+        df[f'收盘MACDsignal_2_{n}_10比值'] = macdsignal/df['收盘']
+        df[f'收盘MACDhist_2_{n}_10比值'] = macdhist/df['收盘']
         macd, macdsignal, macdhist = talib.MACD(
             df['开盘'].values, fastperiod=2, slowperiod=n, signalperiod=10)
-        df[f'开盘MACD_1_{n}_10比值'] = macd/df['开盘']
-        df[f'开盘MACDsignal_1_{n}_10比值'] = macdsignal/df['开盘']
-        df[f'开盘MACDhist_1_{n}_10比值'] = macdhist/df['开盘']
+        df[f'开盘MACD_2_{n}_10比值'] = macd/df['开盘']
+        df[f'开盘MACDsignal_2_{n}_10比值'] = macdsignal/df['开盘']
+        df[f'开盘MACDhist_2_{n}_10比值'] = macdhist/df['开盘']
         macd, macdsignal, macdhist = talib.MACD(
             df['最高'].values, fastperiod=2, slowperiod=n, signalperiod=10)
-        df[f'最高MACD_1_{n}_10比值'] = macd/df['最高']
-        df[f'最高MACDsignal_1_{n}_10比值'] = macdsignal/df['最高']
-        df[f'最高MACDhist_1_{n}_10比值'] = macdhist/df['最高']
+        df[f'最高MACD_2_{n}_10比值'] = macd/df['最高']
+        df[f'最高MACDsignal_2_{n}_10比值'] = macdsignal/df['最高']
+        df[f'最高MACDhist_2_{n}_10比值'] = macdhist/df['最高']
         macd, macdsignal, macdhist = talib.MACD(
             df['最低'].values, fastperiod=2, slowperiod=n, signalperiod=10)
-        df[f'最低MACD_1_{n}_10比值'] = macd/df['最低']
-        df[f'最低MACDsignal_1_{n}_10比值'] = macdsignal/df['最低']
-        df[f'最低MACDhist_1_{n}_10比值'] = macdhist/df['最低']
-
+        df[f'最低MACD_2_{n}_10比值'] = macd/df['最低']
+        df[f'最低MACDsignal_2_{n}_10比值'] = macdsignal/df['最低']
+        df[f'最低MACDhist_2_{n}_10比值'] = macdhist/df['最低']
+        
     df = df.dropna()  # 删除缺失值，避免无效数据的干扰
     for n in range(1, 20):  # 计算未来n日涨跌幅
         df[f'{n}日后总涨跌幅（未来函数）'] = df['收盘'].pct_change(n).shift(-n)*100
