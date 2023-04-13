27c7e28804c58e65818a1f2cb8661b4cae3d2ad7
wth000 <you@example.com>
2023-04-13 02:07:01 +0100

    1

--- Z本地数据统计/概率统计btc.py
+++ Z本地数据统计/概率统计btc.py
@@ -1,62 +1,83 @@
 import pandas as pd
-
-name = 'BTC'
+name='BTC'
+# 读取数据
 df = pd.read_csv(f'{name}指标.csv')
+
+# 去除空值
 df = df.dropna()
 
+# 选取符合交易机会的时间
+df = df[(df['EMA9收盘动能3'] > 1.03) & (df['EMA121收盘比值'] < 0.5)]
 
-# 四均线过滤COIN0.8这里的阈值也应该是动态的，只是多久进行一次调整的问题
-df = df[df['EMA121收盘比值'] <= 0.5].copy()
-df = df[df['EMA121开盘比值'] <= 0.5].copy()
-df = df[df['EMA121最高比值'] <= 0.5].copy()
-df = df[df['EMA121最低比值'] <= 0.5].copy()
+# 按日期排序
+df = df.sort_values('日期')
 
-# 四均线过滤COIN0.8这里的阈值也应该是动态的，只是多久进行一次调整的问题
-df = df[df['EMA9收盘动能3'] >= 1.06].copy()
-df = df[df['EMA9开盘动能3'] >= 1.06].copy()
-df = df[df['EMA9最高动能3'] >= 1.06].copy()
-df = df[df['EMA9最低动能3'] >= 1.06].copy()
+# 转换日期格式
+df['日期'] = pd.to_datetime(df['日期'], format='%Y%m%d')
 
-# 将交易标的细节输出到一个csv文件
-trading_detail_filename = f'{name}交易标的细节.csv'
-df.to_csv(trading_detail_filename, index=False)
+# 计算持仓天数
+hold_days = 10
 
-# 计算每日收益率=100*(100+FJ2-2)/100
-df_daily_return = pd.DataFrame(columns=['日期', '收益率'])
+# 计算每份资金
+cash_per_share = 10000 / hold_days
 
-# 假设开始时有10000元资金,实操时每个月还得归集一下资金，以免收益不平均
-cash_balance = 10000
+# 初始化资产总价值和持仓股票数量
+total_value = 10000
+share_count = 0
 
-# 用于记录每日的资金余额
-daily_cash_balance = {}
+# 初始化交易细节列表和收益率列表
+trading_details = []
+returns = []
 
-n = 6
-for date in df['日期'].unique():
-    group = df[df['日期'] == date]
-    # 如果当日没有入选标的，则单日收益率为0
-    if group.empty:
-        daily_return = 0
-    else:
-        # 计算单日收益率(平均法)
-        group['daily_return'] = group[f'{n}日后总涨跌幅（未来函数）'].mean()/100-0.01
+# 遍历每一天
+for i in range(len(df)):
+    # 获取当天的数据
+    curr_data = df.iloc[i]
 
-        # 计算平均收益率
-        daily_return = group['daily_return'].mean()
+    # 如果当天没有符合交易机会的股票，则跳过
+    if pd.isnull(curr_data['开盘']):
+        continue
 
-    # 更新资金余额并记录每日资金余额
-    cash_balance *= (1 + daily_return)
-    daily_cash_balance[date] = cash_balance
+    # 如果是新的一天则更新持有股票的数量
+    if i > 0 and curr_data['日期'] != df.iloc[i-1]['日期']:
+        share_count = int(total_value / curr_data['开盘'])
+    
+    # 判断是否需要卖出持仓股票
+    if share_count > 0 and i > hold_days:
+        sell_data = df.iloc[i-hold_days-1]
+        total_value = share_count * sell_data['收盘']
+        share_count = 0
+        trading_details.append({
+            '日期': sell_data['日期'].strftime('%Y%m%d'),
+            '操作': '卖出',
+            '股票代码': sell_data['股票代码'],
+            '股票名称': sell_data['股票名称'],
+            '价格': sell_data['收盘'],
+            '数量': hold_days,
+            '金额': total_value,
+        })
+    
+    # 判断是否需要买入新的股票
+    if total_value >= cash_per_share and share_count == 0:
+        buy_data = curr_data
+        share_count = int(cash_per_share / buy_data['开盘'])
+        total_value -= share_count * buy_data['开盘']
+        trading_details.append({
+            '日期': buy_data['日期'].strftime('%Y%m%d'),
+            '操作': '买入',
+            '股票代码': buy_data['股票代码'],
+            '股票名称': buy_data['股票名称'],
+            '价格': buy_data['开盘'],
+            '数量': share_count,
+            '金额': cash_per_share,
+        })
+    
+    # 计算收益率
+    if i > hold_days:
+        returns.append((total_value + share_count * curr_data['收盘']) / 10000 - 1)
+    
+# 输出交易细节
+pd.DataFrame(trading_details).to_csv(f'{name}交易细节.csv', index=False)
 
-    # 记录每日净资产收益率
-    df_daily_return = df_daily_return.append(
-        {'日期': date, '收益率': daily_return}, ignore_index=True)
-
-# 输出每日净资产收益率到csv文件
-df_daily_return.to_csv(f'{name}每日净资产收益率.csv', index=False)
-
-# 将每日资金余额转换为DataFrame
-df_cash_balance = pd.DataFrame(
-    {'日期': list(daily_cash_balance.keys()), '资金余额': list(daily_cash_balance.values())})
-
-# 将每日资金余额输出到csv文件
-df_cash_balance.to_csv(f'{name}每日资金余额.csv', index=False)
+# 输出收益率
+pd.DataFrame({'日期': df['日期'][hold_days:], '收益率': returns}).to_csv(f'{name}收益率.csv', index=False)
