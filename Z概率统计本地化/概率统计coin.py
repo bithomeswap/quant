56201cec70693ae134649d5a6f6525ad3cd33b72
wth000 <you@example.com>
2023-04-15 03:02:43 +0100

    1

--- Z概率统计本地化/概率统计coin.py
+++ Z概率统计本地化/概率统计coin.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-name = 'STOCk'
+name = 'COIN'
 df = pd.read_csv(f'{name}指标.csv')
 
 # 去掉n日后总涨跌幅大于百分之三百的噪音数据
@@ -10,19 +10,18 @@ for n in range(1, 9):
 # 所有行业的超跌阈值0.5
 # df = df[df['EMA121开盘比值'] <= 0.5].copy()
 # # 每日选取动能最强的十标的
-# n_stock = 100
-# df = df.groupby('日期').apply(lambda x: x.nsmallest(
-#     n_stock, 'EMA121开盘比值')).reset_index(drop=True)
-
 n_stock = 100
 df = df.groupby('日期').apply(lambda x: x.nsmallest(
+    n_stock, '开盘')).reset_index(drop=True)
+
+n_stock = 20
+df = df.groupby('日期').apply(lambda x: x.nlargeest(
     n_stock, 'EMA9开盘动能4')).reset_index(drop=True)
 
-# # STOCk价格过滤31，COIN高开低开过滤9.9
-df = df[
-    (df['开盘收盘幅'] <= 8) &
-    (df['开盘收盘幅'] >= 0) 
-]
+# df = df[
+#     (df['开盘收盘幅'] <= 8) &
+#     (df['开盘收盘幅'] >= 0)
+# ]
 
 # 将交易标的细节输出到一个csv文件
 trading_detail_filename = f'{name}交易标的细节.csv'
