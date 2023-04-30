bca9c6b76119adff2f4dc8df183b3d6a9c53d55a
wth000 <you@example.com>
2023-04-30 05:44:16 +0100

    1

--- 数据处理/coin下单测试（未完成）.py
+++ 数据处理/coin下单测试（未完成）.py
@@ -23,71 +23,20 @@ client = Client(api_key, api_secret, testnet=True)
 
 # 连接MongoDB数据库
 client = MongoClient(
-    'mongodb://wth000:wth000@43.159.47.250:27017/dbname?authSource=wth000')
-db = client['wth000']
+    "mongodb://wth000:wth000@43.159.47.250:27017/dbname?authSource=wth000")
+db = client["wth000"]
 name = "BTC"
-collection = db[f'{name}']
-# 获取数据并转换为DataFrame格式
-data = pd.DataFrame(list(collection.find()))
-print("数据读取成功")
+# collection = db[f'{name}']
+# # 获取数据并转换为DataFrame格式
+# data = pd.DataFrame(list(collection.find()))
+# print("数据读取成功")
 collection_write_order = db[f'{name}order']
 collection_write_sell = db[f'{name}sell']
-
-def get_technical_indicators(df):  # 定义计算技术指标的函数
-    # 过滤最高价和最低价为负值的数据
-    df = df.loc[(df['最高'] >= 0) & (df['最低'] >= 0)]
-    df = df.sort_values(by='日期')    # 以日期列为索引,避免计算错误
-    # 计算昨日振幅
-    df['昨日振幅'] = (df.shift(1)['最高']-df.shift(1)['最低'])/df.shift(1)['开盘']
-    # 计算昨日成交额
-    df['昨日成交额'] = df.shift(1)['成交额'].astype(float)
-    # 定义开盘收盘幅
-    df['开盘收盘幅'] = (df['开盘']/df.shift(1)['收盘'] - 1)*100
-    df = df.dropna()  # 删除缺失值，避免无效数据的干扰
-    for n in range(2, 11):
-        df[f'SMA{n*10}开盘比值'] = df['开盘'] / \
-            talib.MA(df['开盘'].values, timeperiod=n*10, matype=0)
-        df[f'SMA{n*10}昨日成交额比值'] = df['昨日成交额'] / \
-            talib.MA(df['昨日成交额'].values, timeperiod=n*10, matype=0)
-        df[f'SMA{n}开盘比值'] = df['开盘'] / \
-            talib.MA(df['开盘'].values, timeperiod=n, matype=0)
-        df[f'SMA{n}昨日成交额比值'] = df['昨日成交额'] / \
-            talib.MA(df['昨日成交额'].values, timeperiod=n, matype=0)
-    for n in range(1, 11):
-        df[f'{n}日后总涨跌幅（未来函数）'] = df['收盘'].shift(-n)/df['收盘']-1
-        df[f'{n*10}日后总涨跌幅（未来函数）'] = df['收盘'].shift(-n*10)/df['收盘']-1
-    return df
-
-
-# 按照“代码”列进行分组并计算技术指标
-grouped = data.groupby('代码').apply(get_technical_indicators)
-
-# 今日筛选股票推送(多头)
-df = grouped.sort_values(by='日期')
-# 获取最后一天的数据
-last_day = df.iloc[-1]['日期']
-# 计算总共统计的股票数量
-code_count = len(df['代码'].drop_duplicates())
-df = df[df[f'日期'] == last_day].copy()
-# 成交额过滤劣质股票
-df = df[df[f'昨日成交额'] >= 20000000].copy()
-# 60日相对超跌
-n_stock = math.ceil(code_count/5)
-df = df.nsmallest(n_stock, f'SMA{60}开盘比值')
-# 振幅较大，趋势明显
-n_stock = math.ceil(code_count/10)
-df = df.nsmallest(n_stock, '昨日振幅')
-# 确认短期趋势
-for n in range(6, 11):
-    df = df[df[f'SMA{n}开盘比值'] >= 1].copy()
-# 开盘价过滤高滑点股票
-df = df[df[f'开盘'] >= 0.01].copy()
-print(len(df))
 
 
 def buy():
     # 首先需要获取所有计划交易的标的，包含symbol、时间、价格、日交易量等等信息
-    symbols = df['代码']
+    symbols = ['BTCUSDT', 'ETHUSDT']
     # 添加异常计数器
     error_count = 0
     for symbol in symbols:
