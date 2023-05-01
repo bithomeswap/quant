95227f92d797691e913601284ea0d5457ce4b636
wth000 <you@example.com>
2023-05-01 05:44:24 +0100

    1

--- 工具栏/coin现货下单.py
+++ 工具栏/coin现货下单.py
@@ -10,6 +10,25 @@ import schedule
 from binance.client import Client
 from pymongo import MongoClient
 
+
+# 函数说明
+# get_exchange_info()：获取当前交易对信息，包括交易对精度、限价单最大值、挂单最小值等。
+# get_order_book(symbol, limit=100)：获取指定交易对的深度信息，包括卖五、买五等信息。
+# get_recent_trades(symbol, limit=500)：获取指定交易对最近成交的交易记录，最多返回500条。
+# get_historical_trades(symbol, limit=500, fromId=None)：获取指定交易对的历史成交记录，最多返回500条，可以指定起始成交记录ID。
+# get_klines(symbol, interval, limit=500, startTime=None, endTime=None)：获取指定交易对和时间区间内的K线数据，包括开盘价、收盘价、最高价、最低价等。
+# get_avg_price(symbol)：获取指定交易对最新的平均价格。
+# get_ticker(symbol)：获取指定交易对最新的价格信息，包括最新成交价、成交量等。
+# get_all_tickers()：获取当前所有交易对的最新价格信息。
+# get_account(recvWindow=60000)：获取当前账户信息，包括余额、冻结资金等。
+# get_open_orders(symbol=None, recvWindow=60000)：获取当前账户的所有未成交的订单信息，可以指定交易对。
+# create_test_order(symbol, side, type, timeInForce, quantity, price=None, stopPrice=None, icebergQty=None, newClientOrderId=None, recvWindow=60000)：下单测试接口，用于测试订单参数是否正确。
+# create_order(symbol, side, type, timeInForce, quantity, price=None, stopPrice=None, icebergQty=None, newClientOrderId=None, recvWindow=60000)：下单接口，用于在指定交易对下单。
+# cancel_order(symbol, orderId=None, origClientOrderId=None, newClientOrderId=None, recvWindow=60000)：取消指定订单，可以通过订单ID或客户端自定义ID来取消。
+# get_order(symbol, orderId=None, origClientOrderId=None, recvWindow=60000)：获取指定订单的信息，可以通过订单ID或客户端自定义ID来查询。
+# get_all_orders(symbol, orderId=None, startTime=None, endTime=None, limit=500, recvWindow=60000)：获取当前账户在指定交易对下的所有订单信息。
+
+
 # # 币安的api配置(主网)
 # api_key = "0jmNVvNZusoXKGkwnGLBghPh8Kmc0klh096VxNS9kn8P0nkAEslVUlsuOcRoGrtm"
 # api_secret = "PbSWkno1meUckhmkLyz8jQ2RRG7KgmZyAWhIF0qPdCJrmDSFxoxGdMG5gZeYYCgy"
@@ -53,28 +72,31 @@ collection_write_sell = db[f'{name}sell']
 #             print(f"卖出{asset}成功！")
 # sell_all()
 
-
 def buy():
     # 获取账户余额
     balances = client.get_account()['balances']
+    print(balances)
     for balance in balances:
         if balance['asset'] == 'USDT':
             usdt_balance = float(balance['free'])
             print('USDT余额：', usdt_balance)
     # 获取计划交易的标的
     symbols = ['BTCUSDT', 'ETHUSDT']
-    print(symbols)
     # 添加异常计数器
     error_count = 0
     for symbol in symbols:
         try:
             symbol_info = client.get_symbol_info(symbol)
-            precision = int(symbol_info['quotePrecision'])
+            # 'pricePrecision': 2：交易对价格小数点位数。
+            # 'quantityPrecision': 3：交易对数量小数点位数。
+            precision = int(symbol_info['quantityPrecision'])  # 获取数量限制
+            price_precision = int(symbol_info['pricePrecision'])  # 获取价格限制
             print(symbol, '下单精度', precision)
+            # {'stepSize': '0.001', 'filterType': 'LOT_SIZE', 'maxQty': '1000', 'minQty': '0.001'}：数量限制条件，包括最小数量、最大数量、数量步长等信息。
             # 最小订单精度
             min_order_value = symbol_info['filters'][1]['minQty']
             min_order_precision = abs(int(math.log10(float(min_order_value))))
-            print(f"最小订单精度: {min_order_precision}")
+            print(f"{symbol}最小订单精度: {min_order_precision}")
             # 实时获取当前卖一和卖二价格
             depth = client.get_order_book(symbol=symbol, limit=5)
             ask_price_1 = float(depth['asks'][0][0])
@@ -219,15 +241,15 @@ def sell():
                 print("无法卖出订单:", order)
 
 
-# 每5分钟执行一次买入操作
-schedule.every(60).minutes.do(buy)
+# # 每5分钟执行一次买入操作
+# schedule.every(60).minutes.do(buy)
 
-# 每分钟执行一次卖出操作
-schedule.every(60).minutes.do(sell)
+# # 每分钟执行一次卖出操作
+# schedule.every(60).minutes.do(sell)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
-# buy()
-# sell()
+buy()
+sell()
