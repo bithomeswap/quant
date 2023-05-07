bf5e9ca7c963adb4336b50e62c9d7cab6474e96d
wth000 <you@example.com>
2023-05-07 09:47:04 +0100

    1

--- 数据获取/get指数指标排名等长度涨跌幅均值.py
+++ 数据获取/get指数指标排名等长度涨跌幅均值.py
@@ -18,7 +18,7 @@ file_path = os.path.join(dir_path, f'{name}指标.csv')
 df = pd.read_csv(file_path)
 # 日期格式转换
 df['日期'] = pd.to_datetime(df['日期'], format='%Y-%m-%d')
-# 去掉n日后总涨跌幅大于百分之三百的噪音数据
+# 去掉噪音数据
 for n in range(1, 9):
     df = df[df[f'{n}日后总涨跌幅（未来函数）'] <= 3*(1+n*0.2)]
 
