7ef6ecc94d860384574c3102ed84808a0c19e913
wth000 <you@example.com>
2023-05-09 09:00:31 +0100

    1

--- 数据获取/getbtc.py
+++ 数据获取/getbtc.py
@@ -45,7 +45,7 @@ for ticker_price in usdt_ticker_prices:
     latest_timestamp = latest_data["timestamp"] if latest_data else 0
     klines = client.futures_klines(
         symbol=symbol,
-        interval=Client.KLINE_INTERVAL_1MINUTE,
+        interval=Client.KLINE_INTERVAL_5MINUTE,
         limit=800
     )
 
