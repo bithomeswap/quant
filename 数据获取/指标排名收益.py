49dfd4317586097d356ae1651f2578f8cf7b0215
wth000 <you@example.com>
2023-05-11 12:13:23 +0100

    1

--- 数据获取/指标排名收益.py
+++ 数据获取/指标排名收益.py
@@ -19,33 +19,61 @@ for name in names:
         for n in range(1, 9):
             df = df[df[f'{n}日后总涨跌幅（未来函数）'] <= 3*(1+n*0.2)]
 
-        if ('证' in name.lower()) and ('分钟' in name.lower()):
-            n = 6  # 设置持仓周期
-        if ('证' in name.lower()) and ('分钟' not in name.lower()):
-            n = 6  # 设置持仓周期
-        if ('coin' in name.lower()) and ('分钟' in name.lower()):
-            n = 6  # 设置持仓周期
         if ('coin' in name.lower()) and ('分钟' not in name.lower()):
+            df = df[df[f'昨日成交额'] >= 1000000].copy()  # 昨日成交额过滤劣质股票
+            df = df[df[f'开盘'] >= 0.00001000].copy()  # 开盘价过滤高滑点股票
+            # 正向
+            df = df[(df['昨日资金贡献_rank'] <= 0.1)].copy()  # 开盘收盘幅过滤涨停无法买入股票
+            df = df[(df['昨日资金波动_rank'] <= 0.1)].copy()  # 开盘收盘幅过滤涨停无法买入股票
+            for n in range(6, 10):  # 对短期趋势上涨进行打分
+                df = df[(df[f'过去{n}日总成交额_rank'] >= 0.8)].copy()
+                df = df[(df[f'过去{n}日资金贡献_rank'] <= 0.2)].copy()
+            print(len(df), name)
             n = 6  # 设置持仓周期
-        mubiao = f'{n}日后总涨跌幅（未来函数）'
-
+            m = 0.003  # 设置手续费
+        # 缩量下跌的转正时刻
+        if ('coin' in name.lower()) and ('分钟' in name.lower()):
+            df = df[df[f'昨日成交额'] >= 10000].copy()  # 成交额过滤劣质股票
+            df = df[df[f'开盘'] >= 0.01].copy()  # 开盘价过滤高滑点股票
+            # 正向
+            df = df[(df[f'昨日涨跌'] >= 1)].copy()
+            df = df[(df['昨日资金波动_rank'] >= 0.5)].copy()
+            for n in range(6, 10):  # 过去几天在下跌
+                df = df[(df[f'过去{n}日总涨跌_rank'] >= 0.5)].copy()
+                df = df[(df[f'过去{n}日资金贡献_rank'] >= 0.8)].copy()
+            print(len(df), name)
+            n = 6  # 设置持仓周期
+            m = 0.0000  # 设置手续费
+        if ('证' in name.lower()) and ('分钟' not in name.lower()):
+            df = df[(df['真实价格'] >= 4)].copy()  # 真实价格过滤劣质股票
+            df = df[(df['开盘收盘幅'] >= -0.08) & (df['开盘收盘幅'] <= 0.01)
+                    ].copy()  # 开盘收盘幅过滤涨停无法买入股票
+            # 以昨日暴涨作为指标的二次确认条件
+            # df = df[(df[f'昨日涨跌'] >= 1.5)].copy()
+            # 正向
+            df = df[(df['昨日资金波动_rank'] <= 0.1)].copy()  # 开盘收盘幅过滤涨停无法买入股票
+            df = df[(df['昨日资金贡献_rank'] <= 0.1)].copy()  # 开盘收盘幅过滤涨停无法买入股票
+            for n in range(6, 10):  # 过去几天在下跌
+                df = df[(df[f'过去{n}日总成交额_rank'] >= 0.8)].copy()
+                df = df[(df[f'过去{n}日资金贡献_rank'] <= 0.2)].copy()
+            print(len(df), name)
+            n = 9  # 设置持仓周期
+            m = 0.005  # 设置手续费
         if ('证' in name.lower()) and ('分钟' in name.lower()):
             df = df[(df['开盘'] >= 4)].copy()  # 真实价格过滤劣质股票
-            df = df[(df['开盘收盘幅'] <= 0.01)].copy()  # 开盘收盘幅过滤涨停无法买入股票
-        # 过滤符合条件的数据
-        if ('证' in name.lower()) and ('分钟' not in name.lower()):
-            df = df[df['真实价格'] >= 4].copy()  # 真实价格过滤劣质股票
-            df = df[df['开盘收盘幅'] <= 0.01].copy()  # 开盘收盘幅过滤涨停无法买入股票
-        if ('coin' in name.lower()) and ('分钟' not in name.lower()):
-            # 过滤低成交的垃圾股
-            df = df[df['昨日成交额'] >= 1000000].copy()
-            # 开盘价过滤高滑点股票
-            df = df[df[f'开盘'] >= 0.00000500].copy()
-        if ('coin' in name.lower()) and ('分钟' in name.lower()):
-            # 过滤低成交的垃圾股
-            df = df[df['昨日成交额'] >= 10000].copy()
-            # 过滤条件(清算规避)：不在0、8、14整点时间之前20分钟的数据（时间戳对应的标准时间）
+            df = df[(df['开盘收盘幅'] >= -0.08) & (df['开盘收盘幅'] <= 0.01)
+                    ].copy()  # 开盘收盘幅过滤涨停无法买入股票
+            # 正向
+            df = df[(df[f'昨日涨跌'] >= 1)].copy()
+            df = df[(df['昨日资金波动_rank'] >= 0.5)].copy()
+            for n in range(6, 10):  # 过去几天在下跌
+                df = df[(df[f'过去{n}日总涨跌_rank'] >= 0.5)].copy()
+                df = df[(df[f'过去{n}日资金贡献_rank'] >= 0.8)].copy()
+            print(len(df), name)
+            n = 9  # 设置持仓周期
+            m = 0.0000  # 设置手续费
 
+        mubiao = f'{n}日后总涨跌幅（未来函数）'
         # 将数据划分成a个等长度的区间
         a = 20
         ranges = []
@@ -54,7 +82,7 @@ for name in names:
         step = (right - left) / a
         for i in range(a):
             ranges.append((left + i * step, left + (i + 1) * step))
-
+        # df[mubiao]=(df[mubiao]+1)*(1-m)-1        # 对目标列进行手续费计算，理论上要计算但是实际上会导致精度过高较难计算
         # 筛选出列名中包含'rank'的列
         rank_cols = df.filter(like='rank').columns.tolist()
         # 创建空的结果DataFrame
@@ -71,9 +99,9 @@ for name in names:
                 # 构造包含指标名和涨跌幅的DataFrame，并添加到列结果DataFrame中
                 result_sub_df = pd.DataFrame(
                     {col_name: [sub_df_mean[mubiao]]}, index=[rank_range])
-                col_result_df = pd.concat([col_result_df, result_sub_df], axis=1)
+                col_result_df = pd.concat(
+                    [col_result_df, result_sub_df], axis=1)
             result_df = pd.concat([result_df, col_result_df])
-            # print(result_df)
         # 保存结果
         result_df.to_csv(f'./涨跌分布/{name}_{n}日后指标排名区间涨跌幅.csv')
         # result_df.to_csv(f'{name}_{n}日后指标排名区间涨跌幅.csv')
