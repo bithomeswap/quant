7e5d468d599cd3d10e34c0cd9bfe28cae71812c3
wth000 <you@example.com>
2023-05-28 02:56:25 +0100

    1

--- 数据获取/工具栏/计算mongodb写入时间.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from bson import ObjectId
-
-# 把 "641c00c06ff2a3207e45b117" 替换成你想查询的 ObjectId 字符串
-oid_str = "6448fa0f4cd8271724205ed4"
-oid = ObjectId(oid_str)
-timestamp = int(str(oid)[:8], 16)  # 获取时间戳，转换为整型
-write_time = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-print(write_time)
