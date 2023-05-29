cf2b7c2a0d38f972ae700f5a66dfb390922d1203
wth000 <you@example.com>
2023-05-29 04:28:44 +0100

    1

--- 交易执行/COIN实盘.py
+++ 交易执行/COIN实盘.py
@@ -34,25 +34,12 @@ def technology(df):  # 定义计算技术指标的函数
         # 删除最高价和最低价为负值的数据
         df.drop(df[(df["最高"] < 0) | (df["最低"] < 0)].index, inplace=True)
         df.sort_values(by="日期")    # 以日期列为索引,避免计算错误
-        # 定义开盘收盘幅
         df["开盘收盘幅"] = df["开盘"]/df["收盘"].copy().shift(1) - 1
-        # 计算涨跌幅
         df["涨跌幅"] = df["收盘"]/df["收盘"].copy().shift(1) - 1
-        # 计算昨日振幅
         df["昨日振幅"] = (df["最高"].copy().shift(
             1)-df["最低"].copy().shift(1))/df["开盘"].copy().shift(1)
-        # 计算昨日成交额
         df["昨日成交额"] = df["成交额"].copy().shift(1)
-        # 计算昨日涨跌
-        df["昨日涨跌"] = df["涨跌幅"].copy().shift(1)+1
-        # 计算昨日资金贡献
-        df["昨日资金贡献"] = df["昨日涨跌"] / df["昨日成交额"]
-        # 计算昨日资金波动
         df["昨日资金波动"] = df["昨日振幅"] / df["昨日成交额"]
-        if ("分钟" in name) | ("指数" in name) | ("行业" in name):
-            for n in range(1, 10):
-                df[f"过去{n}日总涨跌"] = df["开盘"]/(df["开盘"].copy().shift(n))
-                df[f"过去{n*5}日总涨跌"] = df["开盘"]/(df["开盘"].copy().shift(n*5))
     except Exception as e:
         print(f"发生bug: {e}")
     return df
