56222e9136e87db50f712b2e63e272b383638c1c
wth000 <you@example.com>
2023-06-05 11:23:28 +0100

    1

--- 数据获取/getetf.py
+++ 数据获取/getetf.py
@@ -23,9 +23,6 @@ print(codelist)
 df = ak.fund_etf_spot_em()
 df = df[~df["名称"].str.contains("港|纳|H|恒生|标普|黄金|货币|中概")]
 df = df[df["名称"].str.contains(f"{codelist}")]
-df = df[df["总市值"] >= 2000000000]
-# 实盘的时候五亿规模即可，大概四百多个标的，目前十亿不到两百个标的
-print(df)
 # 遍历目标指数代码，获取其分钟K线数据
 for code in df["代码"]:
     # print(code)
@@ -40,7 +37,6 @@ for code in df["代码"]:
         latest_timestamp = latest[0]["timestamp"]
         start_date_query = datetime.datetime.fromtimestamp(
             latest_timestamp).strftime("%Y%m%d")
-
     # 通过 akshare 获取目标指数的日K线数据
     k_data = ak.fund_etf_hist_em(
         symbol=code, start_date=start_date_query, end_date=end_date, adjust="hfq")
@@ -51,12 +47,10 @@ for code in df["代码"]:
         k_data = pd.merge(k_data, k_data_true, on="日期", how="left")
         k_data["代码"] = float(code)
         k_data["成交量"] = k_data["成交量"].apply(lambda x: float(x))
-
         k_data["timestamp"] = k_data["日期"].apply(lambda x: float(
             datetime.datetime.strptime(x, "%Y-%m-%d").replace(tzinfo=pytz.timezone("Asia/Shanghai")).timestamp()))
         # k_data["timestamp"] = k_data["日期"].apply(lambda x: float(
         #     datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S").replace(tzinfo=pytz.timezone("Asia/Shanghai")).timestamp()))
-
         k_data = k_data.sort_values(by=["代码", "日期"])
         docs_to_update = k_data.to_dict("records")
         if upsert_docs:
@@ -87,11 +81,11 @@ for code in df["代码"]:
         print(e, f"因为{code}停牌")
 print("任务已经完成")
 # time.sleep(60)
-limit = 600000
-if collection.count_documents({}) >= limit:
-    oldest_data = collection.find().sort([("日期", 1)]).limit(
-        collection.count_documents({})-limit)
-    ids_to_delete = [data["_id"] for data in oldest_data]
-    collection.delete_many({"_id": {"$in": ids_to_delete}})
-    # 往外读取数据的时候再更改索引吧
-print("数据清理成功")
+# limit = 600000
+# if collection.count_documents({}) >= limit:
+#     oldest_data = collection.find().sort([("日期", 1)]).limit(
+#         collection.count_documents({})-limit)
+#     ids_to_delete = [data["_id"] for data in oldest_data]
+#     collection.delete_many({"_id": {"$in": ids_to_delete}})
+#     # 往外读取数据的时候再更改索引吧
+# print("数据清理成功")
