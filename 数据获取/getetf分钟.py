0f90c46d1108b44607809274678277ea0000397d
wth000 <you@example.com>
2023-06-05 12:42:00 +0100

    1

--- 数据获取/getetf分钟.py
+++ 数据获取/getetf分钟.py
@@ -10,7 +10,7 @@ client = MongoClient(
     'mongodb://wth000:wth000@43.159.47.250:27017/dbname?authSource=wth000')
 db = client['wth000']
 # 设置参数
-name = '分钟ETF'
+name = 'ETF分钟'
 collection = db[f"{name}"]
 # 获取当前日期
 # current_date = datetime.datetime.now()
