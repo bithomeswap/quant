b4625805ab9270fb9f1593ae1807f7ed042821d0
wth000 <you@example.com>
2023-06-11 03:49:47 +0100

    1

--- 数据获取/AI指令/计算器demo/index.js
+++ 数据获取/AI指令/计算器demo/index.js
@@ -1,87 +1,129 @@
-const screen = document.querySelector('.screen');
-const calculator = document.querySelector('#calculator');
-const sqrt = document.querySelector('.sqrt');
-const power = document.querySelector('.power');
-const equal = document.querySelector('.equal');
-const clear = document.querySelector('.clear');
+const operatorRegex = /[-+*/^√]/g;
 
-let expression = '';
+const display = document.getElementById("result");
+const keypad = document.querySelector(".keypad");
 
-function operate(operator) {
-    if (expression && '+-*/^'.includes(expression[expression.length - 1])) {
-        expression = expression.slice(0, -1);
+let currentExpression = "";
+let lastOperatorIndex = -1;
+
+const updateDisplay = () => {
+    display.value = currentExpression;
+};
+
+const calculate = () => {
+    const expression = currentExpression.replace(/^\s+|\s+$/g, "");
+    const operators = expression.match(operatorRegex);
+    let result = 0;
+
+    if (!operators) return;
+
+    for (let i = 0; i < operators.length; i++) {
+        const operator = operators[i];
+        const operatorIndex = expression.indexOf(operator, lastOperatorIndex + 1);
+        const operand1 = parseFloat(expression.slice(lastOperatorIndex + 1, operatorIndex).replace(/\s+/g, ""));
+        const operand2 = parseFloat(expression.slice(operatorIndex + 1, expression.indexOf(operators[i + 1], operatorIndex + 1)).replace(/\s+/g, ""));
+
+        if (operator === "+") {
+            result += operand2;
+        } else if (operator === "-") {
+            result -= operand2;
+        } else if (operator === "*") {
+            result *= operand2;
+        } else if (operator === "/") {
+            result /= operand2;
+        } else if (operator === "^") {
+            result = Math.pow(operand1, operand2);
+        } else if (operator === "√") {
+            result = Math.sqrt(operand2);
+        }
+
+        lastOperatorIndex = operatorIndex;
     }
-    expression += operator;
-    screen.textContent = expression;
-}
 
-function appendNumber(number) {
-    if (expression === '0') {
-        expression = number;
-    } else {
-        expression += number;
+    currentExpression = result.toString();
+    updateDisplay();
+};
+
+const clearAll = () => {
+    currentExpression = "";
+    lastOperatorIndex = -1;
+    updateDisplay();
+};
+
+const backspace = () => {
+    currentExpression = currentExpression.slice(0, -1);
+    lastOperatorIndex = currentExpression.search(operatorRegex) || -1;
+    updateDisplay();
+};
+
+const addDecimal = () => {
+    if (!currentExpression.match(/[-+*/^√]?\d*\.\d*([-+*/^√](?=\d)|$)/g)) {
+        currentExpression += ".";
+        updateDisplay();
     }
-    screen.textContent = expression;
-}
+};
 
-function calculate() {
-    try {
-        const result = eval(expression);
-        screen.textContent = result;
-        expression = result;
-    } catch (err) {
-        console.log(err);
-        screen.textContent = 'Error';
-        expression = '';
-    }
-}
+keypad.addEventListener("click", (event) => {
+    const { target } = event;
 
-function clearScreen() {
-    expression = '';
-    screen.textContent = expression;
-}
-
-function calculateSqrt() {
-    const lastOperatorIndex = expression.search(/[-+*/^]/g);
-    if (lastOperatorIndex !== -1) {
-        const number = expression.slice(lastOperatorIndex + 1);
-        const result = Math.sqrt(Number(number));
-        expression = expression.slice(0, lastOperatorIndex + 1) + result;
-        screen.textContent = expression;
-    } else {
-        const result = Math.sqrt(Number(expression));
-        expression = result;
-        screen.textContent = expression;
-    }
-}
-
-function calculatePower() {
-    const lastOperatorIndex = expression.search(/[-+*/^]/g);
-    if (lastOperatorIndex !== -1) {
-        const number = expression.slice(lastOperatorIndex + 1);
-        const result = Math.pow(Number(number), 2);
-        expression = expression.slice(0, lastOperatorIndex + 1) + result;
-        screen.textContent = expression;
-    } else {
-        const result = Math.pow(Number(expression), 2);
-        expression = result;
-        screen.textContent = expression;
-    }
-}
-
-calculator.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('number')) {
-        appendNumber(target.textContent);
-    } else if (target.classList.contains('operator')) {
-        operate(target.textContent);
-    } else if (target.classList.contains('equal')) {
+    if (target.id === "clear") {
+        clearAll();
+    } else if (target.id === "backspace") {
+        backspace();
+    } else if (target.id === "equals") {
         calculate();
-    } else if (target.classList.contains('clear')) {
-        clearScreen();
-    } else if (target.classList.contains('sqrt')) {
-        calculateSqrt();
-    } else if (target.classList.contains('power')) {
-        calculatePower();
+    } else if (target.classList.contains("operator")) {
+        if (target.id === "squareRoot") {
+            currentExpression += "√";
+            updateDisplay();
+        } else if (!currentExpression.match(/[√][-+*/^√]/g)) {
+            currentExpression += target.textContent;
+            lastOperatorIndex = currentExpression.lastIndexOf(target.textContent);
+            updateDisplay();
+        }
+    } else {
+        currentExpression += target.textContent;
+        updateDisplay();
     }
+});
+
+document.addEventListener("keydown", (event) => {
+    const { key } = event;
+
+    if (/^[0-9]$/.test(key)) {
+        currentExpression += key;
+        updateDisplay();
+    } else if (key === ".") {
+        addDecimal();
+    } else if (/^[\/*\-+^]$/.test(key)) {
+        if (/^[\/*^]$/.test(key)) {
+            lastOperatorIndex = currentExpression.lastIndexOf(key);
+        } else if (/^-/.test(key) && !currentExpression.trim()) {
+            currentExpression += key;
+            updateDisplay();
+            return;
+        } else {
+            lastOperatorIndex = currentExpression.search(operatorRegex);
+        }
+
+        if (lastOperatorIndex === -1) {
+            currentExpression += key;
+            updateDisplay();
+        } else if (operatorRegex.test(currentExpression[lastOperatorIndex + 1])) {
+            currentExpression = `${currentExpression.slice(0, lastOperatorIndex + 1)} ${key} ${currentExpression.slice(lastOperatorIndex + 2)}`;
+            lastOperatorIndex++;
+            updateDisplay();
+        }
+    } else if (key === "Enter") {
+        calculate();
+    } else if (key === "Backspace") {
+        backspace();
+    } else if (key === "Delete") {
+        clearAll();
+    }
+});
+
+display.addEventListener("input", (event) => {
+    currentExpression = event.target.value;
+    lastOperatorIndex = currentExpression.search(operatorRegex) || -1;
 });
