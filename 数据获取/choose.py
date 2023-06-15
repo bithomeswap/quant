49bbd2555d840ade2d90dc22a562a60ebc5d9edb
wth000 <you@example.com>
2023-06-15 02:40:43 +0100

    1

--- 数据获取/choose.py
+++ 数据获取/choose.py
@@ -19,7 +19,8 @@ def technology(df):  # 定义计算技术指标的函数
         else:
             for n in range(1, 46):
                 # 计算不加滑点的收益
-                df[f"{n}日后总涨跌幅（未来函数）"] = (df["收盘"].copy().shift(-n) / df["收盘"]) - 1
+                df[f"{n}日后总涨跌幅（未来函数）"] = (
+                    df["收盘"].copy().shift(-n) / df["收盘"]) - 1
     except Exception as e:
         print(f"发生bug: {e}")
     return df
@@ -55,7 +56,8 @@ def choose(choosename, name, df):
             if ("分钟" not in name):
                 df = df[(df[f"开盘"] >= 0.00000200)].copy()  # 过滤垃圾股
                 df = df[(df[f"过去{1}日资金波动_rank"] <= 0.01)].copy()
-                df = df.groupby("日期", group_keys=True).apply(lambda x: x.nsmallest(1, f"开盘")).reset_index(drop=True)
+                df = df.groupby("日期", group_keys=True).apply(
+                    lambda x: x.nsmallest(1, f"开盘")).reset_index(drop=True)
                 # df = df.groupby("日期", group_keys=True).apply(lambda x: x.nsmallest(1, f"过去{1}日资金波动")).reset_index(drop=True)
                 m = 0.01  # 设置手续费
                 n = 45  # 设置持仓周期
@@ -65,11 +67,12 @@ def choose(choosename, name, df):
                     df = df[(df[f"过去{n}日总涨跌_rank"] >= 0.5)].copy()
                 m = 0.0000  # 设置手续费
                 n = 45  # 设置持仓周期
-        if ("股票" in name)and ("可转债" not in name):  # 数据截取
+        if ("股票" in name) and ("可转债" not in name):  # 数据截取
             if ("分钟" not in name):
                 df = df[(df["开盘"] >= 2) & (df["涨跌幅"] <= 0.09)].copy()  # 过滤垃圾股
                 df = df[(df[f"过去{1}日资金波动_rank"] <= 0.01)].copy()
-                df = df.groupby("日期", group_keys=True).apply(lambda x: x.nsmallest(1, f"开盘")).reset_index(drop=True)
+                df = df.groupby("日期", group_keys=True).apply(
+                    lambda x: x.nsmallest(1, f"开盘")).reset_index(drop=True)
                 # df = df.groupby("日期", group_keys=True).apply(lambda x: x.nsmallest(1, f"过去{1}日资金波动")).reset_index(drop=True)
                 m = 0.000  # 设置手续费
                 n = 30  # 设置持仓周期
@@ -80,6 +83,9 @@ def choose(choosename, name, df):
                 m = 0.0000  # 设置手续费
                 n = 30  # 设置持仓周期
         if ("可转债" in name):
+            df = df[(df["开盘"] <= 110)].copy()  # 过滤垃圾股
+            df = df.groupby("日期", group_keys=True).apply(
+                    lambda x: x.nsmallest(1, f"开盘")).reset_index(drop=True)
             m = 0.0000  # 设置手续费
             n = 30  # 设置持仓周期
         print(len(df), name)
@@ -101,7 +107,7 @@ def choose(choosename, name, df):
                 df = df[(df[f"开盘"] >= 0.00000200)].copy()  # 过滤垃圾股
                 m = 0.0000  # 设置手续费
                 n = 45  # 设置持仓周期
-        if ("股票" in name)and ("可转债" not in name):  # 数据截取
+        if ("股票" in name) and ("可转债" not in name):  # 数据截取
             if ("分钟" not in name):
                 df = df[(df["开盘"] >= 2) & (df["涨跌幅"] <= 0.09)].copy()  # 过滤垃圾股
                 m = 0.005  # 设置手续费
@@ -111,6 +117,7 @@ def choose(choosename, name, df):
                 m = 0.0000  # 设置手续费
                 n = 30  # 设置持仓周期
         if ("可转债" in name):
+            df = df[(df["开盘"] <= 110)].copy()  # 过滤垃圾股
             m = 0.0000  # 设置手续费
             n = 30  # 设置持仓周期
         print(name, n)
